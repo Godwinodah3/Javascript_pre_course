88d6aefd01d7a2fe71e670cbcdcb78a66f5a68cb
Godwin <davegw57@gmail.com>
2023-02-21 21:23:30 +0100

    using break and contine statement

--- coding_pract/loop_iteration/script.js
+++ coding_pract/loop_iteration/script.js
@@ -3,7 +3,6 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
-//looping array
 
 
 
@@ -21,8 +20,48 @@ const dave = [
 const types = [];
 
 for (let i = 0; i < dave.length; i++) {
+	//filling dave array
 	console.log(dave[i], typeof dave[i])
+
+	//filling types of array
 	types[i] = typeof dave[i];
+	types.push(typeof dave[i]);
 }
 
-console.log(types)
+console.log(types);
+
+const years = [1999, 2003, 2005, 2004];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+	2023 - years[i];
+	ages.push(2023 - years[i]);
+}
+console.log(ages);
+
+//continue and break statement
+
+
+//continue
+console.log(`......-------......only_strings......`);
+for (let i = 0; i < dave.length; i++) {
+	if (typeof dave[i] !== 'boolean') continue;
+
+	console.log(dave[i], typeof dave[i])
+
+}
+
+
+//break
+console.log(`......-------......break with number statement......`);
+for (let i = 0; i < dave.length; i++) {
+	if (typeof dave[i] === 'number') break;
+
+	console.log(dave[i], typeof dave[i])
+
+}
+
+
+
+
+lop
